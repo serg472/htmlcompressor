0f15d42f66a820c28071de927dc82e30cb2aa941
serg472 <serg472@dfb84964-0bab-11de-a6e9-d320b29efae0>
2009-03-09 03:50:28 +0000

    Initial release v.0.1

---
+++ src/com/googlecode/htmlcompressor/compressor/Compressor.java
@@ -0,0 +1,31 @@
+package com.googlecode.htmlcompressor.compressor;
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Interface describing compressor classes.
+ * 
+ * @author <a href="mailto:serg472@gmail.com">Sergiy Kovalchuk</a>
+ */
+public interface Compressor {
+	/**
+	 * The main method that compresses the given source and returns a compressed result.
+	 * 
+	 * @param content The content to compress.
+	 * @return Compressed content.
+	 * @throws Exception
+	 */
+	public abstract String compress(String content) throws Exception;
+}
