78c82004d032f693861a0115860374f13c936575
alextunyk@gmail.com <alextunyk@gmail.com@dfb84964-0bab-11de-a6e9-d320b29efae0>
2011-07-06 06:52:19 +0000

    added doc folder generation to local build scripts

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn clean package javadoc:jar javadoc:test-jar source:jar source:test-jar site:jar assembly:single
+mvn clean package javadoc:jar javadoc:test-jar source:jar source:test-jar site:jar assembly:single resources:copy-resources
